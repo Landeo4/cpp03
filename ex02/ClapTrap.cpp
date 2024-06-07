e355c4844153fdb50209ab508774ab39194a9d25
Theo POTILLION <tpotilli@bess-f2r5s12.clusters.42paris.fr>
2024-06-07 13:39:41 +0200

    viral

---
+++ ex02/ClapTrap.cpp
@@ -0,0 +1,81 @@
+#include "ClapTrap.hpp"
+
+ClapTrap:: ClapTrap(std::string Name): _Name(Name)
+{
+	std::cout << "ClapTrap's Constructor called" << std::endl;
+	_HitPoint = 10;
+	_EnergyPoint = 10;
+	_AttackDamage = 0;
+	return ;	
+}
+
+ClapTrap:: ClapTrap(const ClapTrap &nb)
+{
+	*this = nb;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &a)
+{
+	if (this != &a)
+	{
+		this->_Name = a._Name;
+	}
+	return *this;
+}
+
+ClapTrap:: ~ClapTrap(void)
+{
+	std::cout << "ClapTrap's Destructor called" << std::endl;
+	return ;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (this->_EnergyPoint == 0)
+	{
+		std::cout << this->_Name <<" don't have enought energy point" << std::endl;
+		return ;
+	}
+	std::cout << this->_Name << " attack " << target;
+	std::cout << ", causing " << this->_AttackDamage << " points of damage!" << std::endl;
+	this->_EnergyPoint--;
+}
+
+int ClapTrap::getEnergyPoint(void)
+{
+	return this->_EnergyPoint;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_EnergyPoint == 0)
+	{
+		std::cout << this->_Name <<" don't have enought energy point" << std::endl;
+		return ;
+	}
+	std::cout << this->_Name << " take damage";
+	std::cout << ", taking " << amount << " points of damage!" << std::endl;
+	this->_HitPoint = this->_HitPoint - amount;
+	this->_EnergyPoint--;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	long long int result = this->_HitPoint + amount;
+	if (result > 2147483647)
+	{
+		std::cout << "reparation too high, HitPoint set to 10" << std::endl;
+		this->_HitPoint = 10;
+		return ;
+	}
+	if (this->_EnergyPoint == 0)
+	{
+		std::cout << this->_Name << " don't have enought energy point" << std::endl;
+		return ;
+	}
+	std::cout << this->_Name << " repaired by " << amount;
+	std::cout << " now energy point is at ";
+	this->_HitPoint = result;
+	std::cout << this->_HitPoint << std::endl;
+	this->_EnergyPoint--;
+}
